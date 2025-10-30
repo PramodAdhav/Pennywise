5aeb3208dd749272a32773260779dea2e191f23d
JustLikePramod <Pramodadhav111@gmail.com>
2025-10-30 15:11:45 +0530

    Minor Push

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,21 +2,22 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import { viteStaticCopy } from "vite-plugin-static-copy" // ✅ correct import
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    viteStaticCopy({
-      targets: [
-        { src: "public/_redirects", dest: "" }, // copy _redirects into dist
-      ],
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist",
+  },
+  // 👇 This is key
+  server: {
+    historyApiFallback: true,
+  },
+  preview: {
+    historyApiFallback: true,
   },
 })
